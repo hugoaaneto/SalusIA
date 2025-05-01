ab5fc9d447de11fce8ac731e03bd0837fb2e5670
jvvieeira <149310513+jvvieeira@users.noreply.github.com>
2025-05-01 14:59:39 -0300

    Integração com o Ollama rodando via Google Colab.
    https://colab.research.google.com/drive/15-nluvZghBW25JLEL0dmLH4gNODeC73i#scrollTo=3KJQQP3ZvVI5

--- llm_conect.py
+++ llm_conect.py
@@ -7,18 +7,17 @@ def generate_text(prompt, model="llama3.2", reference="", use_rag=False):
     if len(reference) > 1:
         full_prompt = f"{reference}\n\nA partir do texto acima, responda: \n{prompt}. Responda tudo em português."
 
-    url = "http://localhost:11434/api/generate"
-    payload = {"model": model, "prompt": full_prompt, "stream": True}
+    prompt_json_string = json.dumps({"message": full_prompt})
+    payload = {"prompt": prompt_json_string}
 
-    generated_text = ""
+    url = "https://bee0-35-247-136-94.ngrok-free.app/chatbot"  # Lembrar de alterar a URL sempre que rodar o Ollama, pois o NGROK gera um link novo a cada build
 
-    with requests.post(url, json=payload, stream=True) as response:
-        for line in response.iter_lines():
-            if line:
-                try:
-                    data = json.loads(line.decode("utf-8"))
-                    response_text = data.get("response", "")
-                    generated_text += response_text
-                    yield response_text
-                except json.JSONDecodeError:
-                    continue
+    response = requests.post(url, json=payload)
+
+    if response.ok:
+        resposta_json = response.json()
+        reply = resposta_json.get("reply", "")
+        yield reply
+
+    else:
+        yield json.dumps({"error": response.text}, ensure_ascii=False)
